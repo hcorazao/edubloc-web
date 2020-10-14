50428dd1f05399f8604dea199792864267f1d272
JoseErneyOspina <joseerneyosp@gmail.com>
2020-10-14 03:34:07 -0500

    inicio del proyecto

---
+++ ngproject/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SigninComponent } from './components/signin/signin.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SigninComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
